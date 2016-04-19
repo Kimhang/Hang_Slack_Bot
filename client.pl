4fdae3d486768b2424312352cfcdd124db45445e
hang <hang@shou.kr>
2016-04-19 15:21:35 +0900

    auth google token

--- client.pl
+++ client.pl
@@ -6,9 +6,11 @@ use warnings;
 require Hang::Slack::Bot;
 use Data::Printer;
 
-my $client_id = '694170538220-mmc92qgduv2duk4jpodlju9745pkserj.apps.googleusercontent.com';
+my $client_id = '516481921508-k6kuq5o970cish6daf5b3iutj3htbplh.apps.googleusercontent.com';
 my $token = 'xoxb-35491816976-iLjbDXbZrGfQvf8oihXtAQVL';
-my $secret_key = '_SjqEp7ga5jgPYcnz0KF4FY2';
+my $secret_key = 'nX7gYAcPQHvPpjxPLv6B_tzx';
+my $refresh_token = '1/-F1jrY0av0WmGI0Qok4IUw5ybZalVtuY7Y4kmkGeNTkMEudVrK5jSpoR30zcRFq6';
 
-my $self = Hang::Slack::Bot->new( $client_id, $secret_key, $token, '#perl_bot_test' );
+my $self = Hang::Slack::Bot->new( $client_id, $secret_key, $token, $refresh_token, '#perl_bot_test' );
 $self->send_msg('제노스 시작합니다.');
+$self->socket_start;
--- lib/Hang/Slack/Bot.pm
+++ lib/Hang/Slack/Bot.pm
@@ -2,19 +2,22 @@ package Hang::Slack::Bot;
 use utf8;
 use Moose;
 use HTTP::Request::Common qw(POST GET);
-use Furl;
 use IO::Socket;
 #use Net::OAuth2::Profile::WebServer;
 use Net::Google::Calendar;
 use Protocol::WebSocket::Client;
 use JSON qw/encode_json decode_json/;
 use Encode qw/encode_utf8/;
+use LWP::UserAgent;
 use Data::Printer;
+use Data::Dumper;
+
+my $ua = LWP::UserAgent->new;
 
 sub new {
-    my ( $class, $client_id, $secret_key, $token, $channel ) = @_;
+    my ( $class, $client_id, $secret_key, $token, $refresh_token, $channel ) = @_;
 
-    my $res = Furl->new->post( 'https://slack.com/api/rtm.start', [], +{ token => $token } );
+    my $res = $ua->request(POST 'https://slack.com/api/rtm.start', 'Content' => [ token => $token ] );
     die "response fail" unless decode_json($res->content)->{ok};
 
     my $content = decode_json($res->content);
@@ -25,179 +28,131 @@ sub new {
         token      => $token,
         client_id  => $client_id,
         chennel    => $channel,
+        refresh_token => $refresh_token,
         secret_key => $secret_key,
     }, $class;
 }
-#
-#sub socket_start {
-#    my $self = shift;
-#    my $url = $self->{url};
-#
-#    my ($host) = $url =~ m{wss://(.+)/websocket};
-#
-#    my $socket = IO::Socket::SSL->new( PeerHost => $host, PeerPort => 443 );
-#    $socket->blocking(0);
-#    $socket->connect;
-#
-#    my $client = Protocol::WebSocket::Client->new( url => $self->{url} );
-#    $client->on(
-#        read => sub {    
-#            my ( $client, $buf ) = @_;
-#            $self->channels_history;
-#        }
-#    );
-#    $client->on(
-#        write => sub {
-#            my ( $client, $buf ) = @_;
-#            syswrite $socket, $buf;
-#        }
-#    );
-#    $client->on(
-#        connect => sub {
-#            print "on_connect\n";
-#        }
-#    );
-#    $client->on(
-#        error => sub {
-#            my ( $client, $error ) = @_;
-#            print 'on_error: ', $error, "\n";
-#        }
-#    );
-#    
-#    $client->connect;
-#
-#    my $i = 0;
-#    while (1) {
-#        my $data = '';
-#        while ( my $line = readline $socket ) {
-#            $data .= $line;
-#            last if $line eq "\r\n";
-#        }
-#
-#        $client->read($data) if $data;
-#        if ( $i++ % 30 == 0 ) { 
-#            $client->write('{"type": "ping"}');
-#        }
-#
-#        sleep 1;
-#    }
-#}
-#
-#sub channels_history {
-#    my $self = shift;
-#
-#    my $token = $self->{token};
-#    my $channel_id = $self->{channel_id};
-#    my $client_id = $self->{client_id};
-#    my $client_secret = $self->{client_secret};
-#    my $scope = $self->{scope};
-#
-#    my $req = POST 'https://slack.com/api/channels.history',
-#       'Content' => [
-#           token => $token,
-#           channel => $channel_id,
-#           count => 1,
-#        ];
-#    my $res = Furl->new->request($req);
-#    my $json = decode_json(encode_utf8($res->content));
-#    my $subtype = $json->{messages}[0]{subtype};
-#    my $text = $json->{messages}[0]{text};
-#
-#    if ( !$subtype ) {
-#        send_msg( $text, $token );
-#
-#        if ( $text and $text =~ m/calendar/i ) {
-#            $self->authorized;
-#        }elsif ( $text and $text =~ m/^!!(.+)$/i ) {
-#            $self->get_access_token( $1 );
-#        }
-#    }elsif ( $subtype and $subtype eq 'bot_message' ) {
-#        sleep 1;
-#    }
-#}
-#calendar
-#code 복붙
-#나머지 처리
-#명령어를 입력하면 캘린더에 추가
 
-#sub authorized {
-#    my $self = shift;
-#
-#    my $token = $self->{token};
-#    my $client_id = $self->{client_id};
-#    my $client_secret = $self->{client_secret};
-#    my $scope = $self->{scope};
-#
-#    my $oauth2 = Net::Google::DataAPI::Auth::OAuth2->new(
-#            client_id => $client_id,
-#            client_secret => $client_secret,
-#            scope => [ $scope ]
-#    );
-#    my $auth_url = $oauth2->authorize_url();
-#    send_msg( $auth_url, $token );
-#}
-#
-#sub get_access_token {
-#    my ( $self, $code ) = @_;
-#
-#    my $client_id = $self->{client_id};
-#    my $client_secret = $self->{client_secret};
-#
-#    my $redirect_uri = 'urn:ietf:wg:oauth:2.0:oob';
-#    my $token_uri = 'https://accounts.google.com/o/oauth2/token';
-#
-#    my $req = POST $token_uri,
-#       'Content' => [
-#           client_id => $client_id,
-#           client_secret => $client_secret,
-#           redirect_uri => $redirect_uri,
-#           grant_type => 'authorization_code',
-#           code => $code,
-#        ];
-#    my $res = Furl->new->request($req);
-#    my $access_token = decode_json($res->content)->{access_token};
-#    my $refresh_token = decode_json($res->content)->{refresh_token};
-#    my $token_type = decode_json($res->content)->{token_type};
-#
-#    my $ua = Furl->new();
-#    my $api_param = {
-#        client_id => $client_id,
-#        client_secret => $client_secret,
-#        grant_type => 'refresh_token',
-#        refresh_token => $refresh_token,
-#    };
-#    my $response = $ua->post(
-#            $token_uri,
-#            [],
-#            $api_param
-#    );
-#
-#    $response->is_success or die $response->code . " " . $response->message . "\n";
-#
-#    my $body = decode_json($response->body);
-#    $body->{refresh_token} = $refresh_token;
-#    $self->save_access_token(encode_json($body));
-#}
-#
-#sub save_access_token {
-#    my ( $self, $body ) = @_;
-#
-#}
+sub socket_start {
+    my $self = shift;
+    my $url = $self->{url};
+
+    my ($host) = $url =~ m{wss://(.+)/websocket};
+
+    my $socket = IO::Socket::SSL->new( PeerHost => $host, PeerPort => 443 );
+    $socket->blocking(0);
+    $socket->connect;
+
+    my $client = Protocol::WebSocket::Client->new( url => $self->{url} );
+    $client->on(
+        connect => sub {
+            print "on_connect\n";
+        }
+    );
+    $client->on(
+        error => sub {
+            my ( $client, $error ) = @_;
+            print 'on_error: ', $error, "\n";
+        }
+    );
+    $client->on(
+        read => sub {    
+            my ( $client, $buf ) = @_;
+
+            $buf =~ /"type":"(.+)","channel".*"text":"(.+)","ts"/;
+            my ( $msg, $text ) = ( $1, $2 );
+
+            if( $msg eq 'message' and $text =~ /^(?:\*\*)/ ) {
+                $self->check_msg($text);
+            }
+        }
+    );
+    $client->on(
+        write => sub {
+            my ( $client, $buf ) = @_;
+            syswrite $socket, $buf;
+        }
+    );
+    
+    $client->connect;
+
+    my $i = 0;
+    while (1) {
+        my $data = '';
+        while ( my $line = readline $socket ) {
+            $data .= $line;
+            last if $line eq "\r\n";
+        }
+
+        $client->read($data) if $data;
+        if ( $i++ % 30 == 0 ) { 
+            $client->write('{"type": "ping"}');
+        }
+
+        sleep 1;
+    }
+}
+
+sub check_msg {
+    my ( $self, $text ) = @_;
+
+    my ( $order, $detail ) = split( ' ', $text ) if $text;
+
+    my $token = $self->get_token;
+
+##캘린더에 새 이벤트 등록
+    $self->google_calendar($token) if $order =~ /^\*\*calendar$/;
+
+##파일 내려받기
+    $self->get_file($detail) if $order =~ /^\*\*file$/;
+}
+
+sub get_token {
+    my $self = shift;
+
+    my $client_id = $self->{client_id};
+    my $client_secret = $self->{secret_key};
+    my $refresh_token = $self->{refresh_token};
+
+    $ua->timeout(10);
+    $ua->env_proxy;
+
+    my $res = $ua->request(POST 'https://accounts.google.com/o/oauth2/token',
+            'Host'          => 'accounts.google.com',
+            'Content_Type'  => 'application/x-www-form-urlencoded',
+            'Content'       => [
+                'client_id'         =>  $client_id,
+                'client_secret'     =>  $client_secret,
+                'refresh_token'     =>  $refresh_token,
+                'grant_type'        =>  'refresh_token',
+            ],
+        );
+
+    my $access_token = decode_json($res->content)->{access_token};
+
+    return $access_token;
+}
+
+sub google_calendar {
+    my ( $self, $token ) = @_;
+
+}
+
+sub get_file {
+    my ( $self, $detail ) = @_;
+}
 
 sub send_msg {
     my ( $self, $text ) = @_;
 
-    my $req = POST 'https://slack.com/api/chat.postMessage',
+    $ua->request(POST 'https://slack.com/api/chat.postMessage',
        'Content' => [
             token => $self->{token},
             channel => $self->{chennel},
             text => $text,
             as_user => 'true',
-        ];
-    Furl->new->request($req);
+        ],
+    );
 }
-
-
-#명령어를 입력하면 원하는 파일을 저장소에서 갖고오기
 
 1;
